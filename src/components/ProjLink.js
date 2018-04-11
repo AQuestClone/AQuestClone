abfa467114a152252d9622741a0101f94649a264
Tav Hafner <tav.hafner@gmail.com>
2018-04-10 20:49:31 -0600

    portfolio photo hover slides

--- src/components/ProjLink.js
+++ src/components/ProjLink.js
@@ -3,9 +3,42 @@ import glamorous from 'glamorous';
 
 let Photo = glamorous.div(
     {
+        ':hover': {
+            transform: 'translate3d(0px,0,0)'
+        },
+
+        ':before': {
+            position: 'absolute',
+            zIndex: '1',
+            background: 'linear-gradient(to bottom,rgba(51,51,51,0) 0,rgba(51,51,51,.5) 90%)',
+            opacity: '1',
+            transform: 'translate3d(0,50%,0)',
+            transition: 'opacity .35s,transform .35s',
+            left: '0',
+            top: '0',
+            width: '100%',
+            height: '100%',
+            content: '""'
+        },
+        ':after': {
+            position: 'absolute',
+            zIndex: '1',
+            background: 'linear-gradient(to bottom,rgba(51,51,51,.5) 0,rgba(51,51,51,0) 100%)',
+            transition: 'opacity .35s,transform .35s',
+            left: '0',
+            top: '0',
+            width: '100%',
+            height: '100%',
+            content: '""'
+        }
+
+    },
+    {
         backgroundSize: 'cover',
         width: 'calc(100% + 50px)',
-        height: '100%'
+        height: '100%',
+        transform: 'translate3d(-50px,0,0)',
+        transition: 'transform .35s'
     },
     (props) => ({
         background: props.color,
@@ -13,7 +46,7 @@ let Photo = glamorous.div(
     })
 )
 
-let PhotoWrapper = glamorous.div (
+let PhotoWrapper = glamorous.div(
     {
         gridColumnEnd: 'span 2',
         overflow: 'hidden'
@@ -30,7 +63,7 @@ export default class ProjLink extends Component {
             rowSpan
         } = this.props
         return (
-            <PhotoWrapper style={{ gridRowEnd: `span ${rowSpan}`}}>
+            <PhotoWrapper style={{ gridRowEnd: `span ${rowSpan}` }}>
                 <Photo rowSpan={rowSpan} color={color} image={image}>
 
                 </Photo>
