0e0c4b8ddf78e752b22d00a4057162f6b13050a3
Tav Hafner <tav.hafner@gmail.com>
2018-04-11 14:28:46 -0600

    small change with mainwrapper

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -102,7 +102,6 @@ export default class Hamburger extends PureComponent {
 
                                         </div>
                                     ))
-
                                 }
                             </div>
 
